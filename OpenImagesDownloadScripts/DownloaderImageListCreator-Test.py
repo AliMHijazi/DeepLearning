da3b9380606f62821ca6c5a7958bb6dd656cf55b
AliMHijazi <ali_hijazi@hotmail.com>
2023-05-22 18:18:46 -0500

    Adding OpenImages downloader scripts.

---
+++ OpenImagesDownloadScripts/DownloaderImageListCreator-Test.py
@@ -0,0 +1,23 @@
+# This script will take the annotations .csv file and create a text file 
+# called ImageList.txt for use with the Downloader.py script from
+# OpenImages which is available at this URL:
+# https://raw.githubusercontent.com/openimages/dataset/master/downloader.py
+# This will create the downloader file for the TEST set. 
+
+import csv
+csv_file = 'oidv6-train-annotations-bbox.csv'
+
+# Change class_label based on the class label of interest
+# https://storage.googleapis.com/openimages/2017_07/class-descriptions.csv
+class_label = '/m/07jdr' # Railroad Trains
+image_ids = set()
+with open(csv_file, newline='') as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        image_id, label = row[0], row[2]
+        if label == class_label:
+            image_ids.add(image_id)
+with open('ImageList.txt', 'w') as f:
+    for image_id in image_ids:
+        f.write(f'test/{image_id}\n')
---
+++ README.md
@@ -0,0 +1,4 @@
+Deep learning projects and useful scripts:
+
+- OpenImagesDownloaderScripts - For use to download training and testing images from OpenImages
+- RailroadTrainMonitor - Not Complete - A project to monitor a railroad live feed and alert me or something when a train is on the tracks.
