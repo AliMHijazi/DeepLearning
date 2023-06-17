48e77b1e605495bb07d9fffc7416a0a80d63b7f4
AliMHijazi <ali_hijazi@hotmail.com>
2023-06-17 12:31:35 -0500

    Added data plots and other features to StockPredictor.py. Doesn't work well though

--- StockPredictor/StockPredictor.py
+++ StockPredictor/StockPredictor.py
@@ -1,24 +1,37 @@
+# Stock predictor that downloads or uses the previously downloaded full set of closing price data 
+# for the symbol indicated, then uses torch to train and test a model to predict the next set 
+# of price data. Very hard to fine tune and doesn't work great. Requires an Alphavantage API. 
+# To Do: - Create a prediction projection beyond the testing date, but before the current date to 
+# view predictions against actual stock data.
+# - Save the prediction data to a csv in order to log it against actual stock data
+# - Change the x-axis to show the date instead of day.
+# - Create an accompanying script to run this program with different training/testing parameters to 
+#   find the best match. Kind of like the model testing but with additional transparent steps.
+# - Update model architecture to LSTM or GRU
+
 import os
 import numpy as np
 import pandas as pd
 import torch
 import shutil
 from torch import nn
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 import requests
 
-symbol = 'NVDA'
+symbol = 'INTC'
 model_file = f'{symbol}_Model.pth'
 backup_model_file = f'{symbol}_Model_Backup.pth'
 api_key = 'YOUR_API_KEY'
 csv_file = f'{symbol}.csv'
-training_epochs = 100
-training_start = '2021-01-01'
-training_end = '2023-06-01'
+training_epochs = 50
+training_start = '2021-03-01'
+training_end = '2023-01-01'
 testing_start = '2023-01-01'
-testing_end = '2023-06-05'
-num_days = 30 # Number of days to use in the next prediction.
-num_days_to_predict = 5
+testing_end = '2023-06-09'
+num_days = 252 # Number of days to use in the next prediction. 252 trading days per year
+num_neurons = 100
+num_days_to_predict = 20
 
 def get_data(symbol, api_key, csv_file):
     if os.path.exists(csv_file):
@@ -45,11 +58,11 @@ def get_data(symbol, api_key, csv_file):
         data.to_csv(csv_file)
     return data
 
-
-def split_data(data, training_start_date, training_end_date, test_start_date, test_end_date):
-    data_training = data[(data.index >= training_start_date) & (data.index < training_end_date)].copy()
-    data_test = data[(data.index >= test_start_date) & (data.index < test_end_date)].copy()
-    return data_training, data_test
+def split_data(data, training_start, training_end, testing_start, testing_end):
+    data_training = data[(data.index >= training_start) & (data.index < training_end)].copy()
+    data_test = data[(data.index >= testing_start) & (data.index < testing_end)].copy()
+    y_test = data_test['close'].values
+    return data_training, data_test, y_test
 
 def scale_data(data):
     scaler = MinMaxScaler()
@@ -70,6 +83,7 @@ def train_model(model, X_train, y_train, training_epochs):
     optimizer = torch.optim.Adam(model.parameters())
     print('Training model...')
     prev_loss = float('inf')
+    losses = [] 
     for epoch in range(training_epochs):
         epoch_loss = 0
         for i in range(len(X_train)):
@@ -81,11 +95,13 @@ def train_model(model, X_train, y_train, training_epochs):
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+        losses.append(epoch_loss / len(X_train))
         print(f'Epoch {epoch+1}: Loss={epoch_loss/len(X_train)}')
         if abs(prev_loss - epoch_loss) < 1e-5:
             print('Loss stopped decreasing significantly, stopping training early')
             break
         prev_loss = epoch_loss
+    return losses 
 
 def prepare_test_data(data_training, data_test, scaler, num_days):
     past_num_days = data_training.tail(num_days)
@@ -100,32 +116,20 @@ def prepare_test_data(data_training, data_test, scaler, num_days):
     X_test, y_test = np.array(X_test), np.array(y_test)
     X_test = np.squeeze(X_test)
     return X_test
-
-def predict_next_day(model, last_num_days, scaler):
-    inputs = last_num_days
-    inputs = torch.tensor(inputs, dtype=torch.float32)
-    predicted_price = model(inputs).item()
-    return predicted_price
-
-training_start_date = training_start
-training_end_date = training_end
-test_start_date = testing_start
-test_end_date = testing_end
 data = get_data(symbol, api_key, csv_file)
-data_training, data_test = split_data(data, training_start_date, training_end_date, test_start_date, test_end_date)
-scaler = scale_data(data_training)
+data_training, data_test, y_test = split_data(data, training_start, training_end, testing_start, testing_end)
+scaler = MinMaxScaler()
+scaler.fit(data_training[['close']])
 X_train, y_train = prepare_training_data(data_training, num_days)
 X_test = prepare_test_data(data_training, data_test, scaler, num_days)
-#data = get_data(symbol, api_key, csv_file)
 print(f'Obtained {len(data)} rows of data')
-data_training, data_test = split_data(data, training_start, training_end, testing_start, testing_end)
 print(f'Splitting data into {len(data_training)} rows for training and {len(data_test)} rows for testing')
 scaler = scale_data(data_training)
 X_train, y_train = prepare_training_data(data_training, num_days)
 model = nn.Sequential(
-    nn.Linear(num_days, 50),
+    nn.Linear(num_days, num_neurons),
     nn.ReLU(),
-    nn.Linear(50, 1)
+    nn.Linear(num_neurons, 1)
 )
 train_new_model = True
 if os.path.exists(model_file):
@@ -133,25 +137,44 @@ if os.path.exists(model_file):
     if choice.lower() == 'n':
         shutil.copyfile(model_file, backup_model_file)
         print(f'Created backup of existing model: {backup_model_file}')
-        train_new_model = False
+        train_new_model = True
     else:
         state_dict = torch.load(model_file)
         model.load_state_dict(state_dict)
         train_new_model = False
-
 if train_new_model:
-    train_model(model, X_train, y_train, training_epochs)
+    losses = train_model(model, X_train, y_train, training_epochs)
+    plt.plot(losses)
+    plt.title('Training Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.show()
+future_predictions = np.zeros(num_days_to_predict)
+last_window = X_test[-1].reshape(1, -1)
+for i in range(num_days_to_predict):
+    future_pred = model(torch.tensor(last_window, dtype=torch.float32)).detach().numpy()
+    future_pred_rescaled = scaler.inverse_transform(future_pred.reshape(-1, 1)).flatten()[0]
+    future_predictions[i] = future_pred_rescaled
+    last_window = np.roll(last_window, -1)
+    last_window[0][-1] = future_pred
+
 X_test = prepare_test_data(data_training, data_test, scaler, num_days)
 y_pred = model(torch.tensor(X_test, dtype=torch.float32)).detach().numpy()
-print('Model training complete')
-if not os.path.exists(model_file) or choice.lower() == 'n':
-    torch.save(model.state_dict(), model_file)
-    
-last_num_days = data.tail(num_days)
-predicted_prices = []
+y_pred_rescaled = scaler.inverse_transform(y_pred.reshape(-1, 1)).flatten()
+future_predictions = np.zeros(num_days_to_predict)
+last_window = X_test[-1].reshape(1, -1)
 for i in range(num_days_to_predict):
-    predicted_price = predict_next_day(model, last_num_days['close'].values.reshape(1, -1), scaler)
-    predicted_prices.append(round(predicted_price, 2))
-    last_num_days = pd.concat([last_num_days.iloc[1:], pd.DataFrame({'close': [predicted_price]})], ignore_index=True)
-print(f'Predicted stock prices for next {num_days_to_predict} days: {predicted_prices}')
-
+    future_pred = model(torch.tensor(last_window, dtype=torch.float32)).detach().numpy()
+    future_pred_rescaled = scaler.inverse_transform(future_pred.reshape(-1, 1)).flatten()[0]
+    future_predictions[i] = future_pred_rescaled
+    last_window = np.roll(last_window, -1)
+    last_window[0][-1] = future_pred
+plt.plot(y_test, label='Actual')
+plt.plot(y_pred_rescaled, label='Predicted')
+plt.plot(np.arange(len(y_test), len(y_test) + num_days_to_predict), future_predictions, label='Future Predicted')
+plt.title(f'{symbol} Stock Price Prediction')
+plt.xlabel('Day')
+plt.ylabel('Price')
+plt.legend()
+plt.show()
+print(f'Predicted stock prices for next {num_days_to_predict} days: \n{np.round(future_predictions, 2)}')
